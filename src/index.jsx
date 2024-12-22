71619cbe7e4e9b2daff59d588566c25b20f0ca1a
Youssef Jarray <youssefjarray8@gmail.com>
2024-12-22 23:28:33 +0100

    reverted back to original index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import React from "react";
-import App from "./src/App.jsx";
+import App from "App.jsx";
 import { initArrowNavigation } from "@arrow-navigation/react";
 import "./index.css"; // Changed to import from src instead of dist
 import { BrowserRouter } from "react-router-dom";
