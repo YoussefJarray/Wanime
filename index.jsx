08bf8cfaa4e321a7d35eb0d2fc08928b7b7dd87b
Youssef Jarray <youssefjarray8@gmail.com>
2024-12-22 23:20:53 +0100

    pushed potential vercel.json fix

--- index.jsx
+++ index.jsx
@@ -1,20 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import React from "react";
-import App from "./App.jsx";
+import App from "./src/App.jsx";
 import { initArrowNavigation } from "@arrow-navigation/react";
-import "./index.css";
+import "./index.css"; // Changed to import from src instead of dist
 import { BrowserRouter } from "react-router-dom";
-
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 initArrowNavigation();
 
-root.render(<BrowserRouter>
-  <App />
-</BrowserRouter>);
-
-
-
-
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
