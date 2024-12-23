9133ec4aab94ac7663adf1d77c4e5c70d58f8854
Youssef Jarray <youssefjarray8@gmail.com>
2024-12-23 17:53:21 +0100

    pushed potential vercel fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,15 @@ module.exports = {
     }
   },
   plugins: [
-    require('tailwindcss'),
+    require('tailwindcss')({
+      // Ensure the generated CSS is compatible with Vercel
+      purge: {
+        content: ['./*.html', './src/**/*.{js,jsx,ts,tsx}', "./Components/**/*.{js,ts,jsx,tsx}"],
+        options: {
+          safelist: [], // Add any classes you want to keep
+        },
+      },
+    }),
     require('autoprefixer'),
   ],
 };
