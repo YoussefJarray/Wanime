0691e20e7200e86cda7c7bd146b2a67e4f4a5492
Youssef Jarray <youssefjarray8@gmail.com>
2024-12-23 18:04:50 +0100

    pushed potential vercel fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import React from "react";
 import App from "./App.jsx"; // Fixed import path to include './'
 import { initArrowNavigation } from "@arrow-navigation/react";
-import "./index.css"; // Changed to import from src instead of dist
+import "./index.css"; 
 import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
